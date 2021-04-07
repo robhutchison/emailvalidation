a9a2d913fb2579533c0881f9b19f7b7a2bf96af1
Robert Hutchison <Rob@Hutchison-it-solutions.Com>
2021-04-07 14:27:46 +0100

    moved the response model to a more appropriate folder, added comments and allowed for a null id if the registration failed due to errors

--- CustomerRegistrationValidation/RegistrationValidation.Models/RequestModels/CustomerRegistrationResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RegistrationValidation.Models.RequestModels
-{
-	public class CustomerRegistrationResponse
-	{
-		public int CustomerId { get; set; }
-
-		public string[] Errors { get; set; }
-	}
-}
---
+++ CustomerRegistrationValidation/RegistrationValidation.Models/ResponseModels/CustomerRegistrationResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace RegistrationValidation.Models.ResponseModels
+{
+
+	/// <summary>
+	/// The response to the request to register a user
+	/// </summary>
+	public class CustomerRegistrationResponse
+	{
+		/// <summary>
+		/// The customer id if they were successfully registered else null
+		/// </summary>
+		public int? CustomerId { get; set; }
+
+		/// <summary>
+		/// The list of validation errors if there are any
+		/// </summary>
+		public string[] Errors { get; set; }
+	}
+}
